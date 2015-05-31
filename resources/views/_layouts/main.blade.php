c148d70fd0cc90532cf466980d60d3e7b49dfdd0
Yada Khov <yada.khov@gmail.com>
2015-05-31 13:50:10 +0000

    Fix typo

--- resources/views/_layouts/main.blade.php
+++ resources/views/_layouts/main.blade.php
@@ -23,7 +23,7 @@
 
 @yield('content')
 
-<script src="https:F//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 <script src="{{ url('/js/app.js') }}"></script>
 @if (env('APP_ENV') === 'production')
