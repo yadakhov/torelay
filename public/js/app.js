babbc622b13efdf49d3984eee21c588bcc1b4bdd
Yada Khov <yada.khov@gmail.com>
2015-06-01 10:24:09 +0000

    Add random urls

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,19 @@
 $(function() {
 
+    function getIpUrls() {
+        return [
+            "https://ipnumber.info",
+            "http://whatismyip.org",
+            "http://www.findmyip.eu",
+            "http://www.howtofindmyipaddress.com"
+        ];
+    }
+
+    function getRandomUrl() {
+        var urls = getIpUrls();
+        return urls[Math.floor(Math.random() * urls.length)];
+    }
+
     $('#url').focus(function() {
         var url = $('#url').val();
         if (url === 'https://ipnumber.info') {
