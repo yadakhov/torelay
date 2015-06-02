0ccc8678a072f9ceae3fd326e07d2bbb2654f74d
Yada Khov <yada.khov@gmail.com>
2015-06-02 01:52:24 +0000

    Fixes #4 change to use CURLOPT_USERAGENT

--- app/Tor.php
+++ app/Tor.php
@@ -158,17 +158,13 @@ class Tor
 
         $ch = curl_init();
 
-        $headers = [
-            $this->getUserAgent(),  // $this->getUserAgent(true) for random
-        ];
-
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_PROXY, '127.0.0.1:9050');
         curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_VERBOSE, 0);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_USERAGENT, $this->getUserAgent());  // $this->getUserAgent(true) for random);
         $response = curl_exec($ch);
 
         return [$response, $ch];
