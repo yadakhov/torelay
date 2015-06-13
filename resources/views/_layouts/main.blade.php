2bd4e70423fbe69a8aaa9de265a147494428f179
Yada Khov <yada.khov@gmail.com>
2015-06-13 08:10:47 -0400

    Add google analytics

--- resources/views/_layouts/main.blade.php
+++ resources/views/_layouts/main.blade.php
@@ -28,5 +28,14 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 <script src="{{ url('/js/app.js') }}"></script>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-63556433-1', 'auto');
+    ga('send', 'pageview');
+</script>
 </body>
 </html>
